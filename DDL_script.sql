94ee08556ca23660a13213d547e0782a1c6d88e9
Станислав Шушков <80702083+Mazan-ka@users.noreply.github.com>
2023-09-26 23:46:03 +0300

    Create DDL_script.sql

---
+++ DDL_script.sql
@@ -0,0 +1,18 @@
+CREATE table OpenCellId_data (
+	radio String,
+	mcc UInt32,
+	net UInt32,
+	area UInt32,
+	cell UInt64,
+	unit Int64,
+	lon Decimal(10,6),
+	lat Decimal(10,6),
+	range UInt64,
+	samples UInt32,
+	changeable UInt64,
+	created UInt64,
+	updated UInt64,
+	averageSignal UInt64
+) 
+Engine = MergeTree()
+order by (radio, mcc, area, cell);
