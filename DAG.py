256b9fd4b27f4f57d9a35795aa25e2ad99c90a37
Станислав Шушков <80702083+Mazan-ka@users.noreply.github.com>
2023-09-27 17:41:31 +0300

    Update DAG.py

--- DAG.py
+++ DAG.py
@@ -1,9 +1,3 @@
-import requests
-import gzip
-import pandas as pd
-import io
-import clickhouse_connect as click_con
-
 # from airflow import DAG
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta, date
@@ -29,29 +23,38 @@ default_args = {
 def taskflow():
     @task(task_id="extract_data", retries=2)
     def get_df_from_http():
-        url = "https://opencellid.org/ocid/downloads?token=pk.1069fc85f1e203f6a1cb5a28e1d3f57f&type=full&file=cell_towers.csv.gz "
+        import requests
+        import gzip
+        import pandas as pd
+        import io
 
-        # Выполняю GET-запрос с заголовком "Accept-Encoding" для указания поддержки gzip
-        with requests.get(url, headers={"Accept-Encoding": "gzip"}, stream=True) as response:
-            if response.status_code == 200:
-                # Распаковываю сжатые данные
-                with gzip.GzipFile(fileobj=io.BytesIO(response.content), mode='rb') as f:
-                    decoded_content = f.read().decode('utf-8')
+        # Прохожусь циклом по всем нужным мне ссылкам
+        for i in ['262', '460', '310', '208', '510', '404', '250', '724', '234', '311']:
+            url = f"https://opencellid.org/ocid/downloads?token=pk.1069fc85f1e203f6a1cb5a28e1d3f57f&type=mcc&file={i}.csv.gz"
 
-                df = pd.read_csv(io.StringIO(decoded_content), sep=',')
-                df.columns = ['radio', 'mcc', 'net', 'area', 'cell', 'unit', 'lon', 'lat', 'range', 'samples', 'changeable',
-                              'created', 'updated', 'averageSignal']
-                df = df.loc[df['mcc'].isin([262, 460, 310, 208, 510, 404, 250, 724, 234, 311])]
-            else:
-                print("Ошибка при загрузке файла: ", response.status_code)
-            return df
+            # Выполняю GET-запрос с заголовком "Accept-Encoding" для указания поддержки gzip
+            with requests.get(url, headers={"Accept-Encoding": "gzip"}, stream=True) as response:
+                if response.status_code == 200:
+                    # Распаковываю сжатые данные
+                    with gzip.GzipFile(fileobj=io.BytesIO(response.content), mode='rb') as f:
+                        decoded_content = f.read().decode('utf-8')
+                    df = pd.read_csv(io.StringIO(decoded_content), sep=',')
+                    
+                    df.columns = ['radio', 'mcc', 'net', 'area', 'cell', 'unit', 'lon', 'lat', 'range', 'samples', 'changeable',
+                                  'created', 'updated', 'averageSignal']
+                    put_date_into_clickhouse(df)
+                else:
+                    print("Ошибка при загрузке файла: ", response.status_code)
 
-    @task(task_id="Insert_data_into_ch")
+    @task(task_id="Insert_data_into_ch", retries=2)
     def put_date_into_clickhouse(df):
+        import clickhouse_connect as click_con
+        import pandas
+        
         client = click_con.get_client(host='localhost', port=8123, username='', password='')
         client.insert_df('<table-name>', df)
 
-    put_date_into_clickhouse(get_df_from_http())
+    get_df_from_http()
 
 
 taskflow()
