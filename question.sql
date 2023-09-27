bf0210bd7ca5e6d8627762e2259c105760179f87
Станислав Шушков <80702083+Mazan-ka@users.noreply.github.com>
2023-09-27 09:59:06 +0300

    Create question.sql

---
+++ question.sql
@@ -0,0 +1,38 @@
+with isLTE as (
+	select 
+		area,
+		sum(case when radio IN ('LTE') then 0 else 1 end) isLte
+	from 
+		OpenCellId_data 
+	group by area
+	),
+
+cnt_table as (
+	select 
+		area,
+		COUNT(cell) cnt
+	from 
+		OpenCellId_data
+	where 
+		mcc = 250
+	group by area
+	)
+	
+select 
+	area
+from (
+	select *
+	from 
+		cnt_table
+	where 
+		cnt_table.cnt > 100
+	) a
+	
+inner join (
+		 select * 
+		 from 
+		 	isLTE
+		 where 
+		 	isLte > 0
+		 ) b
+	on a.area = b.area
